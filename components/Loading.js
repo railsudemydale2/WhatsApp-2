d659b781d77774a1600c3d12fb8627a455359c4e
dale <stewartdale45@yahoo.com>
2022-08-13 17:41:08 -0400

    Finished completing the Sidebar.js component

--- components/Loading.js
+++ components/Loading.js
@@ -4,7 +4,7 @@ import { Circle } from 'better-react-spinkit';
 
 function Loading() {
   return (
-    <center>
+    <center style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
       <div>
         <img
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
@@ -12,7 +12,7 @@ function Loading() {
           style={{ marginBottom: 10 }}
           height={200}
         />
-        <Circle />
+        <Circle color="#3CBC28" size={60} />
       </div>
     </center>
   );
