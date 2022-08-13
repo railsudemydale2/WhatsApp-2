05b7d142ec214a00bc0037041ed4d39fe7d91e1e
dale <stewartdale45@yahoo.com>
2022-08-13 09:00:57 -0400

    Implementing Styled Components,creating the chat.js page, building the sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,21 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react'
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <Container>
+        <Header><UserAvatar /></Header>
+    </Container>
+  )
+}
+
+export default Sidebar
+
+const Container = styled.div``
+
+const Header = styled.div``
+
+const UserAvatar = styled(Avatar)`
+
+`;
