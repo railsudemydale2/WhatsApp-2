0df282c0d2cb491313d40ebc5c70ea8043f35a66
dale <stewartdale45@yahoo.com>
2022-08-13 11:17:08 -0400

    Implemented the Sidebar component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,22 +1,83 @@
-import { Avatar } from '@material-ui/core';
-import React from 'react'
-import styled from "styled-components";
+import { Avatar, Button, IconButton } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/More';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Sidebar() {
   return (
     <Container>
-        <Header><UserAvatar /></Header>
+      <Header>
+        <UserAvatar />
+
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+
+      <SidebarButton>Start a new chat</SidebarButton>
+
+      {/* List of Chats */}
     </Container>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
 
-const Container = styled.div``
+const Container = styled.div``;
 
-const Header = styled.div``
-
-const UserAvatar = styled(Avatar)`
-
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
 `;
 
+const SidebarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
