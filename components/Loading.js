8be29351783c6b12c24f095f37fb2fa11861ddc1
dale <stewartdale45@yahoo.com>
2022-08-13 14:56:21 -0400

    Building the Loading.js component

--- components/Loading.js
+++ components/Loading.js
@@ -1,9 +1,21 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import { Circle } from 'better-react-spinkit';
 
 function Loading() {
   return (
-    <div>Loading</div>
-  )
+    <center>
+      <div>
+        <img
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+          alt=""
+          style={{ marginBottom: 10 }}
+          height={200}
+        />
+        <Circle />
+      </div>
+    </center>
+  );
 }
 
-export default Loading
+export default Loading;
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,12 @@ import '../styles/globals.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
 import Login from './login';
+import Loading from '../components/Loading';
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
 
-  if (loading) return <Loading />
+  if (true) return <Loading />
   if (!user) return <Login />;
   return <Component {...pageProps} />;
 }
